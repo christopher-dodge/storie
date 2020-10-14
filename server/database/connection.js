25ad98d82ac8e70f6baffc7646741249761ce2c7
Karsten Rabe <karstenrabe91@gmail.com>
2020-10-14 18:41:13 -0400

    fix(deps): add mongodb to deps

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,7 @@
 const express           = require('express');
 const _dotenv           = require('dotenv').config();
 const {MongoClient}     = require('mongodb');
-const URI               = process.env.DATABASEURI;
+const URI               = process.env.DATABASEURI || 'mongodb://127.0.0.1:27017/storie';
 const client            = new MongoClient(URI, {
   useUnifiedTopology: true
 });
