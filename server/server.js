02a8d12a17869f227739119f0e8e7799f5271904
Christopher Dodge <dodge.christopher@icloud.com>
2020-10-23 17:15:28 -0400

    Create startServer function, add pion to handle db connection error, remove dotenv

--- server/server.js
+++ server/server.js
@@ -6,30 +6,42 @@ const api               = require('./routes/api');
 const connectToDatabase = require('./database/connection');
 const app               = express();
 
-// Database connection
-connectToDatabase();
+async function startServer() {
+  // Database connection, will be true or false
+  const dbConnected = await connectToDatabase();
+  console.log('\ndbConnected: ' + dbConnected); 
 
-// Middleware:
-/* istanbul ignore next */
-process.env.NODE_ENV === 'production'
-  ? app.use(morgan('common'))
-  : app.use(morgan('dev'));
+  // Middleware:
+  /* istanbul ignore next */
+  process.env.NODE_ENV === 'production'
+    ? app.use(morgan('common'))
+    : app.use(morgan('dev'));
 
-// Tell node where to serve static files from
-app.use(express.static(path.join(__dirname, '../client/public')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
-});
+  // Tell node where to serve static files from
+  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  });
 
-// API routes
-app.use('/api', api);
+  // API routes
+  app.use('/api', api);
 
-app.use(function (req, res) {
-  res.status(404).send("That's a 404 folks...");
-});
+  app.use(function (req, res) {
+    res.status(404).send("That's a 404 folks...");
+  });
 
-const server = app.listen(PORT).on('listening', () => {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+  // If db is not connected, server will not listen
+  let server;
+  if (dbConnected) {
+    server = app.listen(PORT).on('listening', () => {
+      console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    });
+  } else {
+    console.log('No db connection!');
+  }
+  return server;
+}
+
+const server = startServer();
 
 module.exports = server; // Export for testing
