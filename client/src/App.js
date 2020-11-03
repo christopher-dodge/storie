62f7090e963ed1a2ab58f640cb062ac6ab9b4070
Christopher Dodge <dodge.christopher@icloud.com>
2020-11-03 18:50:48 -0500

    Create GET route in server for all stories, close #27

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,23 @@ import React from 'react';
 import BlogForm from './Components/BlogForm';
 import Header from './Components/Header';
 import Splash from './Pages/Splash';
-// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Stories from './Pages/Stories';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
 
 export const App = () => {
 
   return (
-    // <Router>
-    <div className='App'>
-      <Header />
-      <Splash />
-    </div>
-    // </Router>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Splash} />
+          <Route path="/stories" exact component={Stories} />
+          <Route path="/stories/new" exact component={BlogForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
--- client/src/Pages/Splash/Splash.jsx
+++ client/src/Pages/Splash/Splash.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 
 export default function Splash() {
   return (
-    <div className="center p-top-10">
+    <div className="center" id="splash">
       <h5>Welcome to</h5>
       <h1 className="big-text">Storie</h1>
-      <h5 className="m-bot-5">Share. Experience. Your.</h5>
-
+      <h5 className="m-bot-5">Share. Your. Experience.</h5>
+      <Link to="/stories">
         <Button variant="contained">View Stories</Button>
-
+      </Link>
     </div>
   )
 }
