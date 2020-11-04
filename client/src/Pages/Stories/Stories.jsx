5b389d66f430d6d011ba48cb6f7ee8e69cc0ec45
Christopher Dodge <dodge.christopher@icloud.com>
2020-11-03 20:10:52 -0500

    Create frontend API call to get all stories from the backend, close #35

--- client/src/Pages/Stories/Stories.jsx
+++ client/src/Pages/Stories/Stories.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import API from '../../utils/API';
 
 export default function Stories() {
+  useEffect(() => {
+    getAllStories();
+  }, []);
+
+  const [stories, setStories] = useState([]);
+
+  const getAllStories = async () => {
+    const apiResultObject = await API.getStories();
+    const allFoundStories = apiResultObject.data;
+    
+    console.log(allFoundStories);
+    setStories(allFoundStories);
+  }
+
   return (
     <div>
       <h1>Lots of Stories go here</h1>
+      {stories.map(storie => (
+        <h3>
+          {storie.title}
+        </h3>
+      ))}
     </div>
   )
 }
