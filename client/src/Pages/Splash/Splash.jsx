3cbc17dde13244f05d40872b4352d5c26e97c63c
Christopher Dodge <dodge.christopher@icloud.com>
2020-11-04 19:54:38 -0500

    Touchup Stories page

--- client/src/Pages/Splash/Splash.jsx
+++ client/src/Pages/Splash/Splash.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import '../../App.css';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
 export default function Splash() {
+  // const bgStyle = {
+  //   backgroundImage: url("https://images.unsplash.com/photo-1569073120512-05362a6b92e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80")
+  // };
+
   return (
-    <div className="center" id="splash">
+    <div id="splash" className="full-screen">
       <h5>Welcome to</h5>
       <h1 className="big-text">Storie</h1>
       <h5 className="m-bot-5">Share. Your. Experience.</h5>
--- client/src/Pages/Stories/Stories.jsx
+++ client/src/Pages/Stories/Stories.jsx
@@ -18,10 +18,13 @@ export default function Stories() {
   }
 
   return (
-    <div>
-      <h1>Lots of Stories go here</h1>
+    <div id="stories">
+      <h1 className="m-bot-2">Stories</h1>
       {stories.map((storie, index) => (
-          <StorieCard key={index} storie={storie}/>
+          <StorieCard 
+            key={index} 
+            storie={storie}
+          />
       ))}
     </div>
   )
